f9b645939dc1941290e8a05beb3f33b31a592080
Santeri Salonen <santerisa.salonen@gmail.com>
2021-11-05 16:31:34 +0200

    14-16

---
+++ nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,34 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        # annetaan Statistics-luokan oliolle "stub"-luokan olio
+        self.statistics = Statistics(PlayerReaderStub())
+
+    def test_search(self):
+        palautus = self.statistics.search("Semenko")
+        self.assertAlmostEqual(palautus.name, "Semenko")
+
+    def test_failed_search(self):
+        palautus = self.statistics.search("Moilanen")
+        self.assertAlmostEqual(palautus, None)
+
+    def test_players_in_team(self):
+        palautus = self.statistics.team("EDM")[0]
+        self.assertAlmostEqual(palautus.name, "Semenko")
+
+    def test_top_scores(self):
+        palautus = self.statistics.top_scorers(1)[0]
+        self.assertAlmostEqual(palautus.name, "Gretzky")
