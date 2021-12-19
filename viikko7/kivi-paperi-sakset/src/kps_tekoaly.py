c5a62157dd09c15f6c02472b4a7f28245b89d672
Santeri Salonen <santerisa.salonen@gmail.com>
2021-12-19 17:03:44 +0200

    viikko7

---
+++ viikko7/kivi-paperi-sakset/src/kps_tekoaly.py
@@ -0,0 +1,36 @@
+from tuomari import Tuomari
+from tekoaly import Tekoaly
+
+
+class KPSTekoaly:
+    def pelaa(self):
+        tuomari = Tuomari()
+
+        ekan_siirto = self._ensimmaisen_siirto()
+        tokan_siirto = self._toisen_siirto(ekan_siirto)
+
+        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
+            # ...
+
+        print("Kiitos!")
+        print(tuomari)
+
+    def _ensimmaisen_siirto(self):
+      return input("Ensimmäisen pelaajan siirto: ")
+
+    # tämän metodin toteutus vaihtelee eri pelityypeissä
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        # metodin oletustoteutus
+        return "k"
+
+    def _onko_ok_siirto(self, siirto):
+        return siirto == "k" or siirto == "p" or siirto == "s"
+
+
+# luokka perii luokan KPS
+class KPSPelaajaVsPelaaja(KPS):
+    # toteutetaan metodi pelityypin mukaisesti
+    def _toisen_siirto(self, ensimmaisen_siirto):
+        tokan_siirto = input("Toisen pelaajan siirto: ")
+
+        return tokan_siirto
